07f1cd36dd28cdfbe403ef16fa8df0912b208276
Nick Fausti <ncfausti@gmail.com>
2021-07-02 01:06:45 -0400

    Implement Home with styles from Inv. App.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       colors: {
         sky: colors.sky,
         cyan: colors.cyan,
+        cordGreen: '#269B77',
+        cordPurple: '#4809C6',
+        cordGold: '#efc230',
+        cordGray: {
+          darkest: 'rgb(5,5,5)',
+          darker: 'rgb(20,20,20)',
+          dark: 'rgb(75,75,75)',
+          DEFAULT: '#484848',
+          light: '#e0e6ed',
+          lightest: '#f9fafc',
+        },
       },
     },
   },
